c37a13225fc7bc0ae0dd8e49b426ef07f71f069c
Steph <Inuyasha768@hotmail.com>
2019-06-28 20:29:15 -0400

    ++

--- Where And Why/Where And Why/GameScene.swift
+++ Where And Why/Where And Why/GameScene.swift
@@ -2,5 +2,11 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "background")
+        addChild(background)
+        background.anchorPoint = CGPoint.zero
+        background.position = CGPoint.zero
     }
+    
+    
 }
--- Where And Why/Where And Why/GameViewController.swift
+++ Where And Why/Where And Why/GameViewController.swift
@@ -16,11 +16,22 @@ class GameViewController: UIViewController {
     public var screenWidth: CGFloat {
         return UIScreen.main.bounds.width
     }
-    
+
     // Screen height.
     public var screenHeight: CGFloat {
         return UIScreen.main.bounds.height
     }
+    
+    //Button Positions
+    lazy var X1 = (screenWidth - 320)
+    lazy var X2 = (screenWidth - 160)
+    lazy var Y1 = (screenHeight - 160)
+    lazy var Y2 = (screenHeight - 80)
+    
+    lazy var btnRun = UIButton(frame: CGRect(x: X2, y: Y2, width: 100, height: 50))
+    lazy var btnItems = UIButton(frame: CGRect(x: X1, y: Y2, width: 100, height: 50))
+    lazy var btnSkills = UIButton(frame: CGRect(x: X2, y: Y1, width: 100, height: 50))
+    lazy var btnAttack = UIButton(frame: CGRect(x: X1, y: Y1, width: 100, height: 50))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,39 +44,71 @@ class GameViewController: UIViewController {
         scene.scaleMode = .aspectFill
         skView.presentScene(scene)
         
-        //Button Positions
-        let X1 = (screenWidth - 320)
-        let X2 = (screenWidth - 180)
-        let Y1 = (screenHeight - 140)
-        let Y2 = (screenHeight - 80)
-        
+        let skillsView = SKView()
+        self.view.addSubview(skillsView)
         //Add buttons
-        let btnAttack = UIButton(frame: CGRect(x: X1, y: Y1, width: 100, height: 50))
         btnAttack.backgroundColor = .black
         btnAttack.setTitle("Attack", for: .normal)
-        btnAttack.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        btnAttack.addTarget(self, action: #selector(btnActionAtk), for: .touchUpInside)
 
-        let btnSkills = UIButton(frame: CGRect(x: X2, y: Y1, width: 100, height: 50))
         btnSkills.backgroundColor = .black
         btnSkills.setTitle("Skills", for: .normal)
+        btnSkills.addTarget(self, action: #selector(btnActionSkills), for: .touchUpInside)
         
-        let btnItems = UIButton(frame: CGRect(x: X1, y: Y2, width: 100, height: 50))
         btnItems.backgroundColor = .black
         btnItems.setTitle("Items", for: .normal)
+        btnItems.addTarget(self, action: #selector(btnActionItems), for: .touchUpInside)
         
-        let btnRun = UIButton(frame: CGRect(x: X2, y: Y2, width: 100, height: 50))
         btnRun.backgroundColor = .black
         btnRun.setTitle("Run", for: .normal)
+        btnRun.addTarget(self, action: #selector(btnActionRun), for: .touchUpInside)
         
         //Add buttons to scene?
-        self.view.addSubview(btnAttack)
-        self.view.addSubview(btnSkills)
-        self.view.addSubview(btnItems)
-        self.view.addSubview(btnRun)
+//        self.view.addSubview(btnAttack)
+//        self.view.addSubview(btnSkills)
+//        self.view.addSubview(btnItems)
+//        self.view.addSubview(btnRun)
+        
+        //skills, items, stats hp, enemy, text
+        //makea view for each
+        
+        skillsView.isHidden = true
+        skillsView.addSubview(btnAttack)
+        skillsView.addSubview(btnSkills)
+        skillsView.addSubview(btnItems)
+        skillsView.addSubview(btnRun)
+        //Hide Buttons
+        //HideMainButtons()
     }
 
-    @objc func buttonAction(sender: UIButton!) {
-        print("Button tapped")
+    @objc func btnActionAtk(sender: UIButton!) {
+        print("Pressed")
     }
-
+    
+    @objc func btnActionSkills(sender: UIButton!) {
+        print("Pressed")
+    }
+    
+    @objc func btnActionItems(sender: UIButton!) {
+        print("Pressed")
+    }
+    
+    @objc func btnActionRun(sender: UIButton!) {
+        print("Pressed")
+    }
+    
+    func HideMainButtons(){
+        btnAttack.isHidden = true
+        btnSkills.isHidden = true
+        btnItems.isHidden = true
+        btnRun.isHidden = true
+    }
+    
+    func ShowMainButtons(){
+        btnAttack.isHidden = false
+        btnSkills.isHidden = false
+        btnItems.isHidden = false
+        btnRun.isHidden = false
+    }
+    
 }
